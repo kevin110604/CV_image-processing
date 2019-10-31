2849eb2b0b4fcc4660ac619a9bf1f055cfdc1cbc
Chen Yijia <kevinc110604@gmail.com>
2019-11-01 02:13:16 +0800

    Part two done

--- app.py
+++ app.py
@@ -17,27 +17,44 @@ class AppWindow(QtWidgets.QMainWindow):
         self.ui.pushButton12.clicked.connect(self.pushButton12_Click)
         self.ui.pushButton13.clicked.connect(self.pushButton13_Click)
         self.ui.pushButton14.clicked.connect(self.pushButton14_Click)
+        self.ui.pushButton21.clicked.connect(self.pushButton21_Click)
+        self.ui.pushButton22.clicked.connect(self.pushButton22_Click)
         self.show()
     def pushButton11_Click(self):
-        self.Popup = AppPopup()
-        self.Popup.setGeometry(200, 200, 600, 600)
-        self.Popup.showImg()
-        self.Popup.show()
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.print_h_w()
+        self.popup.showImg()
+        self.popup.show()
     def pushButton12_Click(self):
         self.popup = AppPopup()
         self.popup.setGeometry(200, 200, 600, 600)
         self.popup.colorcvt()
+        self.popup.showImg()
         self.popup.show()
     def pushButton13_Click(self):
-        self.Popup = AppPopup()
-        self.Popup.setGeometry(200, 200, 600, 600)
-        self.Popup.flip()
-        self.Popup.show()
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.flip()
+        self.popup.showImg()
+        self.popup.show()
     def pushButton14_Click(self):
-        self.Popup = AppPopup(1)
-        self.Popup.setGeometry(200, 200, 600, 600)
-        self.Popup.blend()
-        self.Popup.show()
+        self.popup = AppPopup(1)
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.blend()
+        self.popup.show()
+    def pushButton21_Click(self):
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.globalthreshold()
+        # self.popup.showImg()
+        self.popup.show()
+    def pushButton22_Click(self):
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.localthreshold()
+        # self.popup.showImg()
+        self.popup.show()
 
 class AppPopup(QtWidgets.QWidget):
     def __init__(self, choice=0):
@@ -54,40 +71,25 @@ class AppPopup(QtWidgets.QWidget):
             self.sl.setOrientation(QtCore.Qt.Horizontal)
             self.sl.valueChanged.connect(self.blend) 
     def showImg(self):
-        self.img = cv2.imread('images/dog.bmp', cv2.IMREAD_COLOR)
-        height, width, channel = self.img.shape
-        print('Height = %d \nWidth = %d' % (height, width))
         # Change opencv's image to Qimage
+        height, width, channel = self.img.shape
         bytesPerLine = channel * width
         self.qImg = QImage(self.img.data, width, height, bytesPerLine,
                            QImage.Format_RGB888).rgbSwapped()
         # Show Qimage
         self.label.setGeometry(0, 0, width, height)
         self.label.setPixmap(QPixmap.fromImage(self.qImg))
+    def print_h_w(self):
+        self.img = cv2.imread('images/dog.bmp', cv2.IMREAD_COLOR)
+        height, width, channel = self.img.shape
+        print('Height = %d \nWidth = %d' % (height, width))
     def colorcvt(self): 
         self.img = cv2.imread('images/color.png', cv2.IMREAD_COLOR)
         b, g, r = cv2.split(self.img)
         self.img = cv2.merge((g, r, b))
-        #self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2BRG)
-        height, width, channel = self.img.shape
-        # Change opencv's image to Qimage
-        bytesPerLine = channel * width
-        self.qImg = QImage(self.img.data, width, height, bytesPerLine,
-                           QImage.Format_RGB888).rgbSwapped()
-        # Show Qimage
-        self.label.setGeometry(0, 0, width, height)
-        self.label.setPixmap(QPixmap.fromImage(self.qImg))
     def flip(self): 
         self.img = cv2.imread('images/dog.bmp', cv2.IMREAD_COLOR)
         self.img = cv2.flip(self.img, 1)
-        # Change opencv's image to Qimage
-        height, width, channel = self.img.shape
-        bytesPerLine = channel * width
-        self.qImg = QImage(self.img.data, width, height, bytesPerLine,
-                           QImage.Format_RGB888).rgbSwapped()
-        # Show Qimage
-        self.label.setGeometry(0, 0, width, height)
-        self.label.setPixmap(QPixmap.fromImage(self.qImg))
     def blend(self):
         beta = self.sl.value() * 0.01
         alpha = 1 - beta
@@ -100,6 +102,30 @@ class AppPopup(QtWidgets.QWidget):
         bytesPerLine = channel * width
         self.qImg = QImage(self.img.data, width, height, bytesPerLine,
                            QImage.Format_RGB888).rgbSwapped()
+        # Show Qimage
+        self.label.setGeometry(0, 0, width, height)
+        self.label.setPixmap(QPixmap.fromImage(self.qImg))
+    def globalthreshold(self):
+        self.img = cv2.imread('images/QR.png', cv2.IMREAD_GRAYSCALE)
+        ret, self.img = cv2.threshold(self.img, 80, 255, cv2.THRESH_BINARY)
+        # Change opencv's image to Qimage
+        height, width = self.img.shape
+        bytesPerLine = width
+        self.qImg = QImage(self.img.data, width, height, bytesPerLine,
+                           QImage.Format_Grayscale8).rgbSwapped()
+        # Show Qimage
+        self.label.setGeometry(0, 0, width, height)
+        self.label.setPixmap(QPixmap.fromImage(self.qImg))
+    def localthreshold(self):
+        self.img = cv2.imread('images/QR.png', cv2.IMREAD_GRAYSCALE)
+        self.img = cv2.adaptiveThreshold(self.img, 255, 
+                                         cv2.ADAPTIVE_THRESH_MEAN_C,
+                                         cv2.THRESH_BINARY, 19, -1)
+        # Change opencv's image to Qimage
+        height, width = self.img.shape
+        bytesPerLine = width
+        self.qImg = QImage(self.img.data, width, height, bytesPerLine,
+                           QImage.Format_Grayscale8).rgbSwapped()
         # Show Qimage
         self.label.setGeometry(0, 0, width, height)
         self.label.setPixmap(QPixmap.fromImage(self.qImg))
