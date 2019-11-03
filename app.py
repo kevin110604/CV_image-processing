ccbfe11556ebc69f4b6617b0995bbfd49a2085e7
Chen Yijia <kevinc110604@gmail.com>
2019-11-03 08:57:46 +0800

    Part four done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import sys
+from scipy import signal
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import Qt
@@ -19,6 +20,12 @@ class AppWindow(QtWidgets.QMainWindow):
         self.ui.pushButton14.clicked.connect(self.pushButton14_Click)
         self.ui.pushButton21.clicked.connect(self.pushButton21_Click)
         self.ui.pushButton22.clicked.connect(self.pushButton22_Click)
+        self.ui.pushButton31.clicked.connect(self.pushButton31_Click)
+        self.ui.pushButton32.clicked.connect(self.pushButton32_Click)
+        self.ui.pushButton41.clicked.connect(self.pushButton41_Click)
+        self.ui.pushButton42.clicked.connect(self.pushButton42_Click)
+        self.ui.pushButton43.clicked.connect(self.pushButton43_Click)
+        self.ui.pushButton44.clicked.connect(self.pushButton44_Click)
         self.show()
     def pushButton11_Click(self):
         self.popup = AppPopup()
@@ -55,6 +62,46 @@ class AppWindow(QtWidgets.QMainWindow):
         self.popup.localthreshold()
         # self.popup.showImg()
         self.popup.show()
+    def pushButton31_Click(self):
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        angle = int(self.ui.lineEdit311.text())
+        scale = float(self.ui.lineEdit312.text())
+        Tx = int(self.ui.lineEdit313.text())
+        Ty = int(self.ui.lineEdit314.text())
+        self.popup.transformation(angle, scale, Tx, Ty)
+        self.popup.showImg()
+        self.popup.show()
+    def pushButton32_Click(self):
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.localthreshold()
+        # self.popup.showImg()
+        self.popup.show()
+    def pushButton41_Click(self):
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.gaussiansmooth()
+        self.popup.showgrayImg()
+        self.popup.show()
+    def pushButton42_Click(self):
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.sobelx()
+        self.popup.showgrayImg()
+        self.popup.show()
+    def pushButton43_Click(self):
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.sobely()
+        self.popup.showgrayImg()
+        self.popup.show()
+    def pushButton44_Click(self):
+        self.popup = AppPopup()
+        self.popup.setGeometry(200, 200, 600, 600)
+        self.popup.magnitude()
+        self.popup.showgrayImg()
+        self.popup.show()
 
 class AppPopup(QtWidgets.QWidget):
     def __init__(self, choice=0):
@@ -76,6 +123,13 @@ class AppPopup(QtWidgets.QWidget):
         bytesPerLine = channel * width
         self.qImg = QImage(self.img.data, width, height, bytesPerLine,
                            QImage.Format_RGB888).rgbSwapped()
+        # Show Qimage
+        self.label.setGeometry(0, 0, width, height)
+        self.label.setPixmap(QPixmap.fromImage(self.qImg))
+    def showgrayImg(self):
+        # Change opencv's image to Qimage
+        height, width = self.img.shape
+        self.qImg = QImage(self.img.data, width, height, width, QImage.Format_Grayscale8).rgbSwapped()
         # Show Qimage
         self.label.setGeometry(0, 0, width, height)
         self.label.setPixmap(QPixmap.fromImage(self.qImg))
@@ -129,6 +183,96 @@ class AppPopup(QtWidgets.QWidget):
         # Show Qimage
         self.label.setGeometry(0, 0, width, height)
         self.label.setPixmap(QPixmap.fromImage(self.qImg))
+    def transformation(self, angle, scale, Tx, Ty):
+        self.img = cv2.imread('images/OriginalTransform.png', cv2.IMREAD_COLOR)
+        h, w = self.img.shape[:2]
+        # Translation
+        M = np.float32([[1, 0, Tx], [0, 1, Ty]])
+        self.img = cv2.warpAffine(self.img, M, (w, h))
+        # Rotation
+        cx, cy = w // 2, h // 2
+        M = cv2.getRotationMatrix2D((cx, cy), angle, 1.0)
+        self.img = cv2.warpAffine(self.img, M, (w, h))
+        # Scaling
+        rh, rw = int(scale * h), int(scale * w)
+        self.img = cv2.resize(self.img, (rw, rh), interpolation=cv2.INTER_LINEAR)
+    def gaussiansmooth(self):
+        self.img = cv2.imread('images/School.jpg', cv2.IMREAD_COLOR)
+        # R G B
+        self.img = 0.299 * self.img[:, :, 2] + 0.587 * self.img[:, :, 1] + 0.114 * self.img[:, :, 0]
+        # 3*3 Gassian filter
+        x, y = np.mgrid[-1:2, -1:2]
+        gaussian_kernel = np.exp(-(x ** 2 + y ** 2))
+        # Normalization
+        gaussian_kernel = gaussian_kernel / gaussian_kernel.sum()
+        # Gaussian smooth
+        self.img = signal.convolve2d(self.img, gaussian_kernel, mode='same', boundary='fill')
+        self.img = self.img.astype(np.uint8)
+    def sobelx(self):
+        self.img = cv2.imread('images/School.jpg', cv2.IMREAD_COLOR)
+        # R G B
+        self.img = 0.299 * self.img[:, :, 2] + 0.587 * self.img[:, :, 1] + 0.114 * self.img[:, :, 0]
+        # self.img = cv2.Sobel(self.img, cv2.CV_64F, 1, 0)
+        # self.img = np.uint8(np.absolute(self.img))
+        # 3*3 Gassian filter
+        x, y = np.mgrid[-1:2, -1:2]
+        gaussian_kernel = np.exp(-(x ** 2 + y ** 2))
+        # Normalization
+        gaussian_kernel = gaussian_kernel / gaussian_kernel.sum()
+        # Gaussian smooth
+        self.img = signal.convolve2d(self.img, gaussian_kernel, mode='same', boundary='fill')
+        # Sobel Operator
+        X = np.array([[-1, 0, 1],
+                      [-2, 0, 2],
+                      [-1, 0, 1]])
+        # Sobel x
+        self.img = signal.convolve2d(self.img, X, mode='same', boundary='fill')
+        self.img = np.abs(self.img)
+        self.img = self.img.astype(np.uint8)
+    def sobely(self):
+        self.img = cv2.imread('images/School.jpg', cv2.IMREAD_COLOR)
+        # R G B
+        self.img = 0.299 * self.img[:, :, 2] + 0.587 * self.img[:, :, 1] + 0.114 * self.img[:, :, 0]
+        # 3*3 Gassian filter
+        x, y = np.mgrid[-1:2, -1:2]
+        gaussian_kernel = np.exp(-(x ** 2 + y ** 2))
+        # Normalization
+        gaussian_kernel = gaussian_kernel / gaussian_kernel.sum()
+        # Gaussian smooth
+        self.img = signal.convolve2d(self.img, gaussian_kernel, mode='same', boundary='fill')
+        # Sobel Operator
+        Y = np.array([[ 1,  2,  1],
+                      [ 0,  0,  0],
+                      [-1, -2, -1]])
+        # Sobel y
+        self.img = signal.convolve2d(self.img, Y, mode='same', boundary='fill')
+        self.img = np.abs(self.img)
+        self.img = self.img.astype(np.uint8)
+    def magnitude(self):
+        self.imgo = cv2.imread('images/School.jpg', cv2.IMREAD_COLOR)
+        # R G B
+        self.imgo = 0.299 * self.imgo[:, :, 2] + 0.587 * self.imgo[:, :, 1] + 0.114 * self.imgo[:, :, 0]
+        # 3*3 Gassian filter
+        x, y = np.mgrid[-1:2, -1:2]
+        gaussian_kernel = np.exp(-(x ** 2 + y ** 2))
+        # Normalization
+        gaussian_kernel = gaussian_kernel / gaussian_kernel.sum()
+        # Gaussian smooth
+        self.imgo = signal.convolve2d(self.imgo, gaussian_kernel, mode='same', boundary='fill')
+        # Sobel x
+        X = np.array([[-1, 0, 1],
+                      [-2, 0, 2],
+                      [-1, 0, 1]])
+        self.imgx = signal.convolve2d(self.imgo, X, mode='same', boundary='fill')
+        # Sobel y
+        Y = np.array([[ 1,  2,  1],
+                      [ 0,  0,  0],
+                      [-1, -2, -1]])
+        self.imgy = signal.convolve2d(self.imgo, Y, mode='same', boundary='fill')
+        self.img = np.sqrt(self.imgx ** 2 + self.imgy ** 2)
+        self.img -= self.img.min()
+        self.img *= 255.0 / self.img.max()
+        self.img = self.img.astype(np.uint8)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
